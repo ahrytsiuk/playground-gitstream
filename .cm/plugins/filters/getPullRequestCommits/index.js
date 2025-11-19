4f2af6fa820483079e6f046e1988bf401720f392
Andrii Hrytsiuk <andrii.hrytsiuk@gmail.com>
2025-11-19 14:31:59 +0100

    get commits filter

---
+++ .cm/plugins/filters/getPullRequestCommits/index.js
@@ -0,0 +1,35 @@
+const {Octokit} = require("@octokit/rest");
+
+async function getPullRequestCommits(repo, pr, ghToken, callback) {
+
+  const octokit = new Octokit({
+    request: {fetch},
+    auth: ghToken,
+  });
+
+  try {
+    const listCommits = await octokit.paginate(octokit.rest.pulls.listCommits, {
+      owner: repo.owner,
+      repo: repo.name,
+      pull_number: pr.number,
+    });
+    const commits = listCommits.map(item => {
+      return {
+        sha: item.sha,
+        message: item.commit.message,
+        author: item.commit.author.name
+      };
+    });
+
+    return callback(null, commits);
+  } catch (error) {
+    console.error("Error fetching commits:", error);
+    return callback(null, []);
+  }
+}
+
+module.exports = {
+  async: true,
+  immediate: true,
+  filter: getCommits
+}
