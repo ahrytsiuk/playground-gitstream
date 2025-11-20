b4c3c218c84275a3cf90aeb5a13c26d083b24fea
Andrii Hrytsiuk <andrii.hrytsiuk@gmail.com>
2025-11-20 16:59:39 +0100

    from payload

--- .cm/plugins/filters/getPullRequestCommits/index.js
+++ .cm/plugins/filters/getPullRequestCommits/index.js
@@ -1,14 +1,36 @@
 const {Octokit} = require("@octokit/rest");
 
+const getClientPayloadParsed = () => {
+  const afterOneParsing = JSON.parse(process.env.CLIENT_PAYLOAD || '{}');
+
+  if (typeof afterOneParsing === 'string') {
+    return JSON.parse(afterOneParsing);
+  }
+
+  return afterOneParsing;
+};
+
 async function getPullRequestCommits(repo, pr, callback) {
 
   try {
+    const clientPayload = getClientPayloadParsed();
+    const { githubToken, headSha: commit_sha, owner, repo } = clientPayload;
+
+    if (!githubToken || typeof githubToken !== 'string') {
+      console.log(`missing githubToken or bad format`);
+      return callback(null, '[]');
+    }
+
+    const octokit = new Octokit({ auth: githubToken });
+
     const listCommits = await octokit.paginate(octokit.rest.pulls.listCommits, {
       owner: repo.owner,
       repo: repo.name,
       pull_number: pr.number,
       per_page: 100
     });
+
+    core.getIDToken()
 
     console.log(`Successfully fetched ${listCommits.length} commits.`);
 
