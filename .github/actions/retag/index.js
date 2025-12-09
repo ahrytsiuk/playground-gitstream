940bf8264468f3155e74b526c286307d76c5556d
Andrii Hrytsiuk <andrii.hrytsiuk@gmail.com>
2025-12-09 15:04:19 +0100

    Add custom code to print messages in merge.yml

--- .github/actions/retag/index.js
+++ .github/actions/retag/index.js
@@ -1,7 +1,7 @@
 const yaml = require('yaml')
 
 function main() {
-  let docs = yaml.parseAllDocuments('sample.yaml');
+  let docs = yaml.parseAllDocuments(`${process.env.WORKING_DIRECTORY_PATH}/sample.yaml`);
   for (let doc of docs) {
     const asJson = doc.toJS();
     console.log("Document as JSON: ", asJson);
